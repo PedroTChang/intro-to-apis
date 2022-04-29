bce7bc7f4b99b3ad63b05e313779cb77d770df9e
Pedro Chang <pedrochang91@gmail.com>
2022-04-29 12:30:45 -0700

    Creates exchange_calculator.rb

--- exchange_calculator.rb
+++ exchange_calculator.rb
@@ -2,4 +2,14 @@ require "http"
 
 system "Clear"
 puts "This is a currency exchange calculator!"
-puts "The default is dollar, please input your desired "
+puts "Please input the currency you would like to change to first: "
+input_rate = gets.chomp
+puts "Next input your dollar amount: "
+input_usd = gets.chomp.to_i
+
+response = HTTP.get("https://open.er-api.com/v6/latest/USD")
+
+exchanges = response.parse(:json)["rates"]
+# pp exchanges
+
+puts response.parse(:json)["rates"][input_rate] * input_usd

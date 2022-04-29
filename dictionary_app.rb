d3170f35c3e63ea84e7e2efa021677e015b084d9
Pedro Chang <pedrochang91@gmail.com>
2022-04-29 10:49:50 -0700

    Adds top_example to dictionary_app.rb

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -4,11 +4,24 @@ system "clear"
 puts "Welcome to the Dictionary App!"
 puts "Enter a word: "
 word = gets.chomp
+system "clear"
 
-response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
+dict_entry = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
 
-dict_entry = response.parse(:json)
-definition = dict_entry[3]["text"]
+definition = dict_entry.parse(:json)[3]["text"]
 
+###
+
+examples = HTTP.get("https://api.wordnik.com/v4/word.json/ostentatious/topExample?useCanonical=false&api_key=")
+
+top_example = examples.parse(:json)["text"]
+
+puts "This is the definition:"
 puts definition
-# starting to ask for top example and pronunciation
+puts "\n"
+puts "And here is the top example:"
+puts top_example
+puts "\n"
+# # starting to ask for top example and pronunciation
+# # pp dict_entry.parse(:json)
+# pp examples.parse
